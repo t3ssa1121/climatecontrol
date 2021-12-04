902d0e831d4843ab209ef2187352e4117038bcb7
dl21273@essex.ac.uk <dleece@firstfiretech.ca>
2021-12-04 01:24:04 -0700

    v0 of client

--- ssat3_manode/manodeTestClient.py
+++ ssat3_manode/manodeTestClient.py
@@ -13,7 +13,69 @@
 ############################################################
 
 import  paho.mqtt.client as paho
-import json, sys, datetime,random
+import json, sys, datetime,random,time
+from os import environ, path
+from dotenv import load_dotenv
 
 
-print("deps met")
+def setvars():
+    # Load node specific data using environment file
+    basedir = path.abspath(path.dirname(__file__))
+    load_dotenv(path.join(basedir,'.env'))
+    SECRET_KEY=environ.get('SKEY128')
+    NODEID=environ.get('MANID')
+    QID=environ.get('QID')
+    QPWD=environ.get('QPWD')
+    QHOST=environ.get('QHOST')
+    QPORT=environ.get('QPORT')
+    return[SECRET_KEY,NODEID,QID,QPWD,QHOST,QPORT]
+
+def newclient(nodeid,uid,pwd):
+    manodeid='manode-{}'.format(nodeid)
+    mqclient=mqtt.Client(manodeid)
+    mqclient.username_pw_set(username=uid,password=pwd)
+    # Add TLS settings when ready
+    return mqclient
+
+def newconnect(mqclient,mqhost,mqport):
+    mqconnect=mqclient.connect(mqhost,port=mqport,keepalive=120)
+    #mqconnect=mqclient.connect(mqhost)
+    return mqconnect
+
+def on_connect(client,userdata,flags,rc):
+    # confirm connection worked, print response code
+    if rc==0:
+        print("Successful connection to broker")
+    else:
+        print("Connection failed, rc: " +connack_strin(rc))
+
+def topicpub(connected,topic,nodeid,data):
+    topic='{}/{}'.format(topic,nodeid)
+    connected.publish(topic,data)
+    return
+
+def newtemp():
+    curtemp=str(round(random.uniform(10.00,35.00),2))
+    return curtemp
+
+
+
+
+
+def main():
+    nodevars=setvars()
+    thisclient=newclient(nodevars[1],nodevars[2],nodevars[3])
+    thisconnect=newconnect(thisclient,nodevars[4],nodevars[5])
+    # test loop
+    thisconnect.loop_start()
+    while True:
+        tempdata=newtemp()
+        topicpub(thisconnect,"ct",nodevars[1],tempdata)
+        time.sleep(random.randint(5,45))
+
+
+    
+
+
+if __name__=="__main__":
+    main()
