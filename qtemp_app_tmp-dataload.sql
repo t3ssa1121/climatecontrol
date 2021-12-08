0478f8af7ba3a210371fdfe0bce0c2af1dc046c6
dl21273@essex.ac.uk <dleece@firstfiretech.ca>
2021-12-07 21:33:01 -0700

    Built SQL schema and data load scripts

---
+++ qtemp_app_tmp-dataload.sql
@@ -0,0 +1,5 @@
+INSERT INTO nodedatatmp (manodeguid) VALUES ('5ea489d1-9a6e-4718-a485-d35fd2e526ae')
+INSERT INTO nodedatatmp (manodeguid) VALUES ('617985f7-4d40-4b26-9d79-b958fa5bd7c6')
+INSERT INTO nodedatatmp (manodeguid) VALUES ('7273b5fe-be3a-4728-b365-567e86f10abc')
+INSERT INTO nodedatatmp (manodeguid) VALUES ('1f94d059-f3d8-4ad0-b0a3-b12c8f025b60')
+INSERT INTO nodedatatmp (manodeguid) VALUES ('72621444-8c8c-4cd0-869d-d86380fcdfa8')
