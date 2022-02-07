146d80e1dedf616c0f928ba44f96efd8e48eb237
t3ssa1121 <94329998+t3ssa1121@users.noreply.github.com>
2022-02-06 18:38:48 -0700

    Delete wifiiot directory

--- wifiiot/controller/pubmqtt1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Temp work around while writing the python publisher
-
-while true ; do
-
-    timestamp=`date +%R:%m:%S.%N`
-    /usr/bin/mosquitto_pub -h 127.0.0.1 -p 4321 -t "fieldsite/ctrl_beacon" -m ${timestamp}
-    sleep 15
-    done
